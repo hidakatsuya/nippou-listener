0ceb58dc43fd58467de3df3b751ae151e51bec4c
Katsuya Hidaka <hidakatsuya@gmail.com>
2019-12-01 18:52:04 +0900

    deploy

---
+++ precache-manifest.3551fc1029a3ffc5da32d38b5cacf7c4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b78ad0c8778ce7faee",
+    "url": "/nippou-player/css/about.8914d7e7.css"
+  },
+  {
+    "revision": "00170f3dd197508db5ba",
+    "url": "/nippou-player/css/app.80196e68.css"
+  },
+  {
+    "revision": "ff1da79ee61cf39f28c6",
+    "url": "/nippou-player/css/chunk-vendors.4bea3433.css"
+  },
+  {
+    "revision": "1382c29cdb72f6c99043675d6e13b625",
+    "url": "/nippou-player/fonts/photon-entypo.1382c29c.ttf"
+  },
+  {
+    "revision": "2614e058b2dcb9d6e2e964730d795540",
+    "url": "/nippou-player/fonts/photon-entypo.2614e058.eot"
+  },
+  {
+    "revision": "bf614256dbc49f4bf2cf786706bb0712",
+    "url": "/nippou-player/fonts/photon-entypo.bf614256.woff"
+  },
+  {
+    "revision": "060be840840b8fd8810bc35916d6e4cb",
+    "url": "/nippou-player/img/logo.060be840.png"
+  },
+  {
+    "revision": "d45e5708ecc33aa7e7e333e23d7147a7",
+    "url": "/nippou-player/index.html"
+  },
+  {
+    "revision": "f6b78ad0c8778ce7faee",
+    "url": "/nippou-player/js/about.3614387d.js"
+  },
+  {
+    "revision": "00170f3dd197508db5ba",
+    "url": "/nippou-player/js/app.414d4508.js"
+  },
+  {
+    "revision": "ff1da79ee61cf39f28c6",
+    "url": "/nippou-player/js/chunk-vendors.fcdfd55a.js"
+  },
+  {
+    "revision": "b88889ed51e3105d40b0e567159a3bd6",
+    "url": "/nippou-player/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nippou-player/robots.txt"
+  }
+]);
